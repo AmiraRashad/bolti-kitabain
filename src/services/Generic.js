a70dfb92f92b5fe97465fd467727da5a7d76860c
Amira Rashad <amira.rashad1996@hotmail.com>
2021-08-16 15:36:57 +0500

    api

--- src/services/Generic.js
+++ src/services/Generic.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//axios.defaults.baseURL="http://localhost:4000/api/";
-axios.defaults.baseURL="https://layers-app-api.herokuapp.com/api";
+axios.defaults.baseURL="http://localhost:3001";
+// axios.defaults.baseURL="https://layers-app-api.herokuapp.com/api";
 
 axios.defaults.headers.common["x-auth-token"]=localStorage.getItem("token");
 
