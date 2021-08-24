efd2a6f0799099fdbeee18ef57d6e012a6a20dfe
imhamza1 <hamza.akram4635@gmail.com>
2021-08-24 15:24:17 +0500

    feat: eslint added and removed all errors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
-import Gridpage from "./Components/Grid/Grid";
-import Signin from "./Components/SignIn/SignIn";
-import Signup from "./Components/SignUp/Signup";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavBar from "./Components/NavBar/Navbar";
-import AudioPlayer from "./Components/AudioPlayer/AudioPlayer";
-import tracks from "./Components/AudioPlayer/tracks";
-function App() {
-  return (
-    <Router>
-      <NavBar />
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GridPage from './Components/GridPage/GridPage';
+import SignIn from './Components/SignIn/SignIn';
+import Signup from './Components/SignUp/Signup';
+import NavBar from './Components/NavBar/NavBar';
+import AudioPlayer from './Components/AudioPlayer/AudioPlayer';
+import tracks from './Components/AudioPlayer/tracks';
 
-      <Switch>
-        <Route path="/signin" component={Signin} />
-        <Route path="/signup" component={Signup} />
-        <Route
-          path="/audioplayer"
-          render={() => <AudioPlayer tracks={tracks} />}
-        />
-        <Route path="/" component={Gridpage} />
-      </Switch>
-    </Router>
-  );
+function App() {
+	return (
+		<Router>
+			<NavBar />
+			<Switch>
+				<Route path="/signin" component={SignIn} />
+				<Route path="/signup" component={Signup} />
+				<Route
+					path="/audioplayer"
+					render={() => <AudioPlayer tracks={tracks} />}
+				/>
+				<Route path="/" component={GridPage} />
+			</Switch>
+		</Router>
+	);
 }
 export default App;
--- src/Components/AudioPlayer/AudioPlayer.jsx
+++ src/Components/AudioPlayer/AudioPlayer.jsx
@@ -1,150 +1,148 @@
-import React, { useState, useEffect, useRef } from "react";
-import AudioControls from "./AudioControls";
-import Backdrop from "./BackDrop";
-import "./styles.css";
+import React, { useState, useEffect, useRef } from 'react';
+import AudioControls from './AudioControls';
+import Backdrop from './BackDrop';
+import './styles.css';
 
 /*
  * Read the blog post here:
  * https://letsbuildui.dev/articles/building-an-audio-player-with-react-hooks
  */
+
 const AudioPlayer = ({ tracks }) => {
-  // State
-  const [trackIndex, setTrackIndex] = useState(0);
-  const [trackProgress, setTrackProgress] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
+	// State
+	const [trackIndex, setTrackIndex] = useState(0);
+	const [trackProgress, setTrackProgress] = useState(0);
+	const [isPlaying, setIsPlaying] = useState(false);
 
-  // Destructure for conciseness
-  const { title, artist, color, image, audioSrc } = tracks[trackIndex];
+	// Destructure for conciseness
+	const { title, artist, color, image, audioSrc } = tracks[trackIndex];
 
-  // Refs
-  const audioRef = useRef(new Audio(audioSrc));
-  const intervalRef = useRef();
-  const isReady = useRef(false);
+	// Refs
+	const audioRef = useRef(new Audio(audioSrc));
+	const intervalRef = useRef();
+	const isReady = useRef(false);
 
-  // Destructure for conciseness
-  const { duration } = audioRef.current;
+	// Destructure for conciseness
+	const { duration } = audioRef.current;
 
-  const currentPercentage = duration
-    ? `${(trackProgress / duration) * 100}%`
-    : "0%";
-  const trackStyling = `
+	const currentPercentage = duration
+		? `${(trackProgress / duration) * 100}%`
+		: '0%';
+	const trackStyling = `
     -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))
   `;
+	const toNextTrack = () => {
+		if (trackIndex < tracks.length - 1) {
+			setTrackIndex(trackIndex + 1);
+		} else {
+			setTrackIndex(0);
+		}
+	};
+	const startTimer = () => {
+		// Clear any timers already running
+		clearInterval(intervalRef.current);
 
-  const startTimer = () => {
-    // Clear any timers already running
-    clearInterval(intervalRef.current);
+		intervalRef.current = setInterval(() => {
+			if (audioRef.current.ended) {
+				toNextTrack();
+			} else {
+				setTrackProgress(audioRef.current.currentTime);
+			}
+		}, [1000]);
+	};
 
-    intervalRef.current = setInterval(() => {
-      if (audioRef.current.ended) {
-        toNextTrack();
-      } else {
-        setTrackProgress(audioRef.current.currentTime);
-      }
-    }, [1000]);
-  };
+	const onScrub = (value) => {
+		// Clear any timers already running
+		clearInterval(intervalRef.current);
+		audioRef.current.currentTime = value;
+		setTrackProgress(audioRef.current.currentTime);
+	};
 
-  const onScrub = (value) => {
-    // Clear any timers already running
-    clearInterval(intervalRef.current);
-    audioRef.current.currentTime = value;
-    setTrackProgress(audioRef.current.currentTime);
-  };
+	const onScrubEnd = () => {
+		// If not already playing, start
+		if (!isPlaying) {
+			setIsPlaying(true);
+		}
+		startTimer();
+	};
 
-  const onScrubEnd = () => {
-    // If not already playing, start
-    if (!isPlaying) {
-      setIsPlaying(true);
-    }
-    startTimer();
-  };
+	const toPrevTrack = () => {
+		if (trackIndex - 1 < 0) {
+			setTrackIndex(tracks.length - 1);
+		} else {
+			setTrackIndex(trackIndex - 1);
+		}
+	};
 
-  const toPrevTrack = () => {
-    if (trackIndex - 1 < 0) {
-      setTrackIndex(tracks.length - 1);
-    } else {
-      setTrackIndex(trackIndex - 1);
-    }
-  };
+	useEffect(() => {
+		if (isPlaying) {
+			audioRef.current.play();
+			startTimer();
+		} else {
+			audioRef.current.pause();
+		}
+	}, [isPlaying]);
 
-  const toNextTrack = () => {
-    if (trackIndex < tracks.length - 1) {
-      setTrackIndex(trackIndex + 1);
-    } else {
-      setTrackIndex(0);
-    }
-  };
+	// Handles cleanup and setup when changing tracks
+	useEffect(() => {
+		audioRef.current.pause();
 
-  useEffect(() => {
-    if (isPlaying) {
-      audioRef.current.play();
-      startTimer();
-    } else {
-      audioRef.current.pause();
-    }
-  }, [isPlaying]);
+		audioRef.current = new Audio(audioSrc);
+		setTrackProgress(audioRef.current.currentTime);
 
-  // Handles cleanup and setup when changing tracks
-  useEffect(() => {
-    audioRef.current.pause();
+		if (isReady.current) {
+			audioRef.current.play();
+			setIsPlaying(true);
+			startTimer();
+		} else {
+			// Set the isReady ref as true for the next pass
+			isReady.current = true;
+		}
+	}, [trackIndex]);
 
-    audioRef.current = new Audio(audioSrc);
-    setTrackProgress(audioRef.current.currentTime);
+	useEffect(() => {
+		// Pause and clean up on unmount
 
-    if (isReady.current) {
-      audioRef.current.play();
-      setIsPlaying(true);
-      startTimer();
-    } else {
-      // Set the isReady ref as true for the next pass
-      isReady.current = true;
-    }
-  }, [trackIndex]);
+		audioRef.current.pause();
+		clearInterval(intervalRef.current);
+	}, []);
 
-  useEffect(() => {
-    // Pause and clean up on unmount
-    return () => {
-      audioRef.current.pause();
-      clearInterval(intervalRef.current);
-    };
-  }, []);
-
-  return (
-    <div style={{ marginTop: 100 }} className="audio-player">
-      <div className="track-info">
-        <img
-          className="artwork"
-          src={image}
-          alt={`track artwork for ${title} by ${artist}`}
-        />
-        <h2 className="title">{title}</h2>
-        <h3 className="artist">{artist}</h3>
-        <AudioControls
-          isPlaying={isPlaying}
-          onPrevClick={toPrevTrack}
-          onNextClick={toNextTrack}
-          onPlayPauseClick={setIsPlaying}
-        />
-        <input
-          type="range"
-          value={trackProgress}
-          step="1"
-          min="0"
-          max={duration ? duration : `${duration}`}
-          className="progress"
-          onChange={(e) => onScrub(e.target.value)}
-          onMouseUp={onScrubEnd}
-          onKeyUp={onScrubEnd}
-          style={{ background: trackStyling }}
-        />
-      </div>
-      <Backdrop
-        trackIndex={trackIndex}
-        activeColor={color}
-        isPlaying={isPlaying}
-      />
-    </div>
-  );
+	return (
+		<div style={{ marginTop: 100 }} className="audio-player">
+			<div className="track-info">
+				<img
+					className="artwork"
+					src={image}
+					alt={`track artwork for ${title} by ${artist}`}
+				/>
+				<h2 className="title">{title}</h2>
+				<h3 className="artist">{artist}</h3>
+				<AudioControls
+					isPlaying={isPlaying}
+					onPrevClick={toPrevTrack}
+					onNextClick={toNextTrack}
+					onPlayPauseClick={setIsPlaying}
+				/>
+				<input
+					type="range"
+					value={trackProgress}
+					step="1"
+					min="0"
+					max={duration}
+					className="progress"
+					onChange={(e) => onScrub(e.target.value)}
+					onMouseUp={onScrubEnd}
+					onKeyUp={onScrubEnd}
+					style={{ background: trackStyling }}
+				/>
+			</div>
+			<Backdrop
+				trackIndex={trackIndex}
+				activeColor={color}
+				isPlaying={isPlaying}
+			/>
+		</div>
+	);
 };
 
 export default AudioPlayer;
