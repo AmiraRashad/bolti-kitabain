29890d2713efbed26467627dc8e83a1d8181f76a
AmiraRashad <amira.rashad1996@hotmail.com>
2021-08-24 19:49:27 +0500

    typescript

--- src/Components/Auth/Auth.tsx
+++ src/Components/Auth/Auth.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import* as React from "react";
 import { withRouter } from "react-router-dom";
 import { useStyles } from "./styles";
 
 import userService from "../../services/UserService";
-const Auth = (props) => {
+const Auth = (props:any) => {
   React.useEffect(() => {
     if (!userService.isLoggedin()) {
       props.history.push("/login");
     }
   }, []);
-  return <>{props.children}</>;
+  return {props}; // children removed
 };
 
 export default withRouter(Auth);
